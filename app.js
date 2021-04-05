646805aed136efd6852c283a41d82521dc0edec6
Kate <kate.matveeva@gmail.com>
2021-04-06 00:55:49 +0300

    refactor: 3.1- 3.3,adv 2.1 ul,li class definition

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var completedTasksHolder = document.getElementById("completed-tasks");//complete
 var createNewTaskElement = function (taskString) {
 
     var listItem = document.createElement("li");
+    listItem.className = 'task-block';
 
     //input (checkbox)
     var checkBox = document.createElement("input");//checkbx
@@ -33,7 +34,7 @@ var createNewTaskElement = function (taskString) {
     var deleteButtonImg = document.createElement("img");//delete button image
 
     label.innerText = taskString;
-    label.className = 'task';
+    label.className = 'task-block__text task';
 
     //Each elements, needs appending
     checkBox.type = "checkbox";
